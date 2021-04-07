4daa36206394afa887fb60fd4bc7181b70314286
Santo DeRose <derosesanto95@gmail.com>
2021-04-07 15:09:34 -0500

    Updated Gem file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
 
     def trash_em
         @trash_em = List.where(:completed => true).destroy_all
-        redirect_to lists_url, notice: 'All Marked Items Were Successfully Deleted.' 
+        redirect_to root_url, notice: 'All Marked Items Were Successfully Deleted.' 
     end
     
     def trash_em_all
         @trash_em_all = List.all.destroy_all
-        redirect_to lists_url, notice: 'All Items Were Successfully Deleted.' 
+        redirect_to root_url, notice: 'All Items Were Successfully Deleted.' 
     end
     
     
